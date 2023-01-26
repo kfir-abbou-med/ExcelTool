2aea2db0c58a73db6aa5dc8dfc2e7fb7b34a636b
kfir abbou <79653524+kfir-abbou@users.noreply.github.com>
2023-01-26 16:11:56 +0200

    Writing title after creating new file with pivot data

--- main.py
+++ main.py
@@ -10,15 +10,21 @@ def read_excel(file):
     return df
 
 
+def set_titles(df, col_name):
+    df.at[0, col_name] = 'kfkfkfkfkfk'
+    return df
+
+
 def main():
     df = read_excel(f'{excel_dir}\\1.xlsx')
     cost_centers = df['Cost Center'].tolist();
     cost_centers = set(cost_centers)
+    tmp_output_file = str(f'{excel_dir}\\tmp_out.xlsx')
     output_file = str(f'{excel_dir}\\out.xlsx')
     pivots = {}
 
     # Create pivot data and write to file
-    with pd.ExcelWriter(output_file, engine='openpyxl') as writer_pivot:
+    with pd.ExcelWriter(tmp_output_file, engine='openpyxl') as writer_pivot:
         for cc in cost_centers:
             pivots[cc] = df[df['Cost Center'] == cc].pivot_table(index=['Cost Element', 'Cost element name'],
                                                                  columns=['Period'], values=['Val/COArea Crcy'],
@@ -27,16 +33,20 @@ def main():
             center_int = int(cc)
             pivots[center_int].to_excel(writer_pivot, sheet_name=center)
 
-    out_df = pd.read_excel(output_file, sheet_name=0)  # can also index sheet by name or fetch all sheets
-    with pd.ExcelWriter(output_file, engine='openpyxl', mode='a') as writer:
-        for sheet in cost_centers:
-            for column in out_df:
-                col_width = max(out_df[column].astype(str).map(len).max(), len(column))
-                col_idx = out_df.columns.get_loc(column)
-                col_letter = openpyxl.utils.cell.get_column_letter(col_idx + 1)
-                writer.sheets[str(sheet)].column_dimensions[str(col_letter)].width = col_width
+    out_df = pd.read_excel(tmp_output_file)  # can also index sheet by name or fetch all sheets
 
+    with pd.ExcelWriter(tmp_output_file, engine='openpyxl', mode='a', if_sheet_exists="overlay") as writer:
+        out_df = set_titles(out_df, 'Unnamed: 0')
+        out_df.to_excel(writer, index=False)
 
+    print(out_df)
+    out_df.to_excel(tmp_output_file, index=False, header=False)
+    #     for sheet in cost_centers:
+
+            # for column in out_df:
+            #     col_width = max(out_df[column].astype(str).map(len).max(), len(column))
+            #     col_idx = out_df.columns.get_loc(column)
+            #     col_letter = openpyxl.utils.cell.get_column_letter(col_idx + 1)
+            #     writer.sheets[str(sheet)].column_dimensions[str(col_letter)].width = col_width
+            # out_df.to_excel(writer, index=False)
 main()
-# for cc in cost_centers:
-#     writer.sheets[str(cc)].column_dimensions['A'].width = 100
